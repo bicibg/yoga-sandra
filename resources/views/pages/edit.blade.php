974a4503cc066ea5a6a46f4e2d3d7dc4ebbc680e
Bugra Ergin <bugra.ergin@payrexx.com>
2019-06-21 17:07:01 +0200

    tinymce editor

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -14,14 +14,19 @@
                     <label for="title">Titel</label>
                     <input type="text" class="form-control" name="title" id="title" value="{{$page->title}}">
                 </div>
+                @if($page->image)
+                    <div class="form-group text-center">
+                        <img src="{{asset('storage/'.$page->image)}}" alt="image" style="width:50%;">
+                    </div>
+                @endif
+
                 <div class="form-group">
                     <label for="image">Hintergrundbild</label>
                     <input type="file" class="form-control" name="image" id="image" value="{{$page->image}}">
                 </div>
                 <div class="form-group">
                     <label for="content">Inhalt</label>
-                    <input id="content" type="hidden" name="content" value="{{$page->content}}">
-                    <trix-editor input="content"></trix-editor>
+                    <textarea name="content" id="content" cols="30" rows="50">{{$page->content}}</textarea>
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-success">Aktualisieren</button>
@@ -41,13 +46,28 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.1/trix.css">
 @endsection
 @section('script')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.1/trix.js"></script>
+    <script src="https://cdn.tiny.cloud/1/j53xdzposqqlnom9q27xd3u13c3j6l9jgdofqj2vriz116w6/tinymce/5/tinymce.min.js"></script>
+    <script>
+        tinymce.init({
+            selector:'#content',
+            plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount tinymcespellchecker a11ychecker imagetools textpattern help formatpainter permanentpen pageembed tinycomments mentions linkchecker',
+            toolbar: 'formatselect | bold italic strikethrough forecolor backcolor permanentpen formatpainter | link image media pageembed | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | removeformat | addcomment',
+            image_advtab: true,
+            template_cdate_format: '[CDATE: %m/%d/%Y : %H:%M:%S]',
+            template_mdate_format: '[MDATE: %m/%d/%Y : %H:%M:%S]',
+            image_caption: true,
+            spellchecker_dialog: true,
+            tinycomments_mode: 'embedded',
+            content_style: '.mce-annotation { background: #fff0b7; } .tc-active-annotation {background: #ffe168; color: black; }'
+        });
+    </script>
+
     <script type="text/javascript">
         jQuery(document).ready(function($){
             var content = "";
             setInterval(function(){
-                if(content !== $("input#content[type=hidden]").val()){
-                    content = $("input#content[type=hidden]").val();
+                if(content !== $("#content").val()){
+                    content = $("#content").val();
                     $('#preview').html(content);
                 }
             }, 1000);
