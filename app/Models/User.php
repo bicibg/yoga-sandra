5eacbf0a1d179668a09d163edd3c5e06417b36ad
Bugra Ergin <bugra.ergin@hotelcard.com>
2025-02-09 16:47:28 +0100

    filament user.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -32,7 +32,10 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return true;
+    }
     /**
      * Get the attributes that should be cast.
      *
